f713a50081ab61f89cbf849a6113fd99f08eb184
Bgeekprg <bhaveshpr54@gmail.com>
2025-01-21 12:54:20 +0530

    fix:Invalid user choice

--- Calculator.py
+++ Calculator.py
@@ -1,35 +1,57 @@
 # Menu for showing operation list
 def menu():
-    print("1. + ")
-    print("2. - ")
-    print("3. * ")
-    print("4. / ")
-    print("5. % ")
-    print("6. ^ ")
-#Calculating Operations
-def calculate(choice,num1,num2):
-    if choice==1:
-        return num1+num2
-    elif choice==2:
-        return num1-num2
-    elif choice==3:
-        return num1*num2
-    elif choice==4:
-        return num1/num2
-    elif choice==5:
-        return num1%num2
-    elif choice==6:
-        return num1**num2
-        
-    else:
-        return "Enter valid Choice"
+    print("1. Addition ")
+    print("2. Subtraction ")
+    print("3. Multiplication ")
+    print("4. Division ")
+    print("5. Modulus ")
+    print("6. Exponent ")
 
-#calling Menu 
+# Calculating Operations
+def calculate(choice, num1, num2):
+    if choice == 1:
+        return num1 + num2
+
+    elif choice == 2:
+        return num1 - num2 
+
+    elif choice == 3:
+        return num1 * num2  
+
+    elif choice == 4:
+        if num2 != 0:
+            return num1 / num2
+        else:
+            return "Error: Division by zero"
+    elif choice == 5:
+        return num1 % num2 
+
+    elif choice == 6:
+        return num1 ** num2 
+    
+# Calling Menu
 menu()
-choice=int(input("Enter Ur Choice = "))
-num1=eval(input("Enter num 1 ="))
-num2=eval(input("Enter num 2 ="))
 
-# Passing parameters in calculate method and print return value
-print(calculate(choice,num1,num2))
+choice=""
 
+try:
+    # Getting input of selection of menu item.
+    choice = int(input("Enter Your Choice = "))
+
+    #checks choice is in range 1 to 6
+    if choice < 1 or choice > 6:
+        raise Exception()
+
+except ValueError:
+    print("Enter integer value in choice !")
+
+except:
+    print("You entered wrong choice !")
+
+else:
+    # Getting inputs of two numbers.
+    num1 = eval(input("Enter num 1 = "))
+    num2 = eval(input("Enter num 2 = "))
+
+    # Passing parameters in calculate method and print return value
+    print(calculate(choice, num1, num2))
